165f04f63ba8a207e4ba0d4d952354eac433c23d
synthels <synthels.me@gmail.com>
2021-05-07 17:38:12 +0300

    Formatting...

--- kernel/drivers/driver.h
+++ kernel/drivers/driver.h
@@ -7,11 +7,11 @@
 #define DRIVER_NO_WRITE -1
 
 struct driver_interface {
-	char		*name;
-	int			(*write)	(uint8_t *);
-	void		(*read)		(uint8_t *);
-	int			enabled;
-	int			event; /* Set if an event is raised (SHOULD ONLY BE SET BY THE DRIVER & CLEARED BY THE KERNEL) */
+	char *name;
+	int (*write) (uint8_t *);
+	void (*read) (uint8_t *);
+	int enabled;
+	int event; /* Set if an event is raised (SHOULD ONLY BE SET BY THE DRIVER & CLEARED BY THE KERNEL) */
 };
 
 #endif
