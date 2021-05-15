e7d7c3be6865d5fd72f7971c97550a7e3d521dc9
synthels <synthels.me@gmail.com>
2021-05-15 18:28:55 +0300

    [boot] forgot I tried editing the bootsector

--- kernel/boot/boot.s
+++ kernel/boot/boot.s
@@ -1,23 +1,11 @@
-;;
-; Copyright (C) 2020 synthels <synthels.me@gmail.com>
-;
-; This program is free software; you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation; either version 2 of the License, or
-; (at your option) any later version.
-;
-; This program is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; GNU General Public License for more details.
-;
-; Bootsector
-;;
+##
+# Bootsector
+##
 
-extern load_gdt
-global setcs
+.extern load_gdt
+.global setcs
 
-; multiboot header constants
+# multiboot header constants
 .set ALIGN,    1<<0
 .set MEMINFO,  1<<1
 .set FLAGS,    ALIGN | MEMINFO  # flags
