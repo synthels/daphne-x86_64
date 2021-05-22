a6abcc292db29e9a5c1105751ad82f24434ffddd
synthels <synthels.me@gmail.com>
2021-05-22 17:26:27 +0300

    [io] add outl and inl

--- kernel/io/io.c
+++ kernel/io/io.c
@@ -21,11 +21,25 @@ void outb(uint16_t port, uint8_t val)
 	asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
 }
 
+void outl(uint16_t port, uint8_t val)
+{
+    asm volatile ( "outl %%eax, %%dx": :"d" (port), "a" (val) );
+}
+
 uint8_t inb(uint16_t port)
 {
 	uint8_t ret;
 	asm volatile ( "inb %1, %0"
 		: "=a"(ret)
 		: "Nd"(port) );
+	return ret;
+}
+
+uint8_t inl(uint16_t port)
+{
+	uint8_t ret;
+	asm volatile ( "inl %%dx, %%eax"
+		: "=a"(ret)
+		: "d"(port) );
 	return ret;
 }
--- kernel/io/io.h
+++ kernel/io/io.h
@@ -18,7 +18,9 @@
 #include <stdint.h>
 
 void outb(uint16_t port, uint8_t val);
+void outl(uint16_t port, uint8_t val);
 
 uint8_t inb(uint16_t port);
+uint8_t inl(uint16_t port);
 
 #endif
