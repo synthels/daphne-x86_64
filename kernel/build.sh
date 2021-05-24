17d88b1c303df96b0950d8c3c65a267ff44e5964
synthels <synthels.me@gmail.com>
2021-05-24 15:31:09 +0300

    [other] organized build scripts

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,30 +2,6 @@
 
 # Kernel build script
 
-# Point this to your nasm binary
-NASM="nasm"
-# Point this to your gcc binary
-GCC="i686-elf-gcc"
-# Point this to your qemu binary
-QEMU="qemu-system-i386"
-
-# Point this to your as binary
-AS="i686-elf-as"
-
-# Build directory name
-BUILD_DIR="build"
-# Set this to your target platform
-ARCH_TARGET="x86_32"
-
-# Overwrite grub.cfg
-echo "default=0
-timeout=15
-
-menuentry \"phiOS\" {
-	multiboot /boot/kernel.bin
-	boot
-}" > $BUILD_DIR/boot/grub/grub.cfg
-
 # Assemble bootsector
 $AS kernel/arch/$ARCH_TARGET/boot/boot.s -o $BUILD_DIR/boot.o
 # Assemble IDT
@@ -35,37 +11,14 @@ $NASM -f elf32 kernel/arch/$ARCH_TARGET/boot/gdt.s -o $BUILD_DIR/gdt.o
 # Assemble paging code
 $NASM -f elf32 kernel/arch/$ARCH_TARGET/boot/paging.s -o $BUILD_DIR/paging.o
 
-function compileModule() {
-	for file in $(find kernel/$1 -name '*.c'); do
-		echo "Compiling kernel source file: $file..."
-		$GCC -g -c $file -o $BUILD_DIR/"${file//\/}".o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Ilib/include -Ikernel -Wno-maybe-uninitialized
-	done
-}
-
-compileModule "arch/$ARCH_TARGET"
-compileModule "kmain"
-compileModule "io"
-compileModule "idt"
-compileModule "drivers"
-compileModule "tty"
-compileModule "memory"
-compileModule "logger"
-
-echo "Linking..."
+KernelSubModule "arch/$ARCH_TARGET"
+KernelSubModule "kmain"
+KernelSubModule "io"
+KernelSubModule "idt"
+KernelSubModule "drivers"
+KernelSubModule "tty"
+KernelSubModule "memory"
+KernelSubModule "logger"
 
 # Link everything together
 $GCC -g -T kernel/linker.ld -o $BUILD_DIR/boot/kernel.bin -ffreestanding -O2 -nostdlib $BUILD_DIR/*.o -lgcc
-
-# Clean build directory
-rm $BUILD_DIR/*.o
-
-# Parse arguments
-for arg in "$@"; do
-	if [ $arg == "-iso" ]; then
-		echo "Building ISO..."
-		grub-mkrescue -o phiOS.iso $BUILD_DIR
-	elif [ $arg == "-qemu" ]; then
-		read -p "Press ENTER to run qemu"
-		$QEMU -kernel $BUILD_DIR/boot/kernel.bin -soundhw pcspk
-	fi
-done
--- phiOS.sh
+++ phiOS.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
 
+#########################################
+# Point this to your nasm binary
+NASM=nasm
+
+# Point this to your gcc binary
+GCC=i686-elf-gcc
+
+# Point this to your qemu binary
+QEMU=qemu-system-i386
+
+# Point this to your as binary
+AS=i686-elf-as
+
+# Set this to your target platform
+ARCH_TARGET=x86_32
+
 # Build directory name
-BUILD_DIR="build"
+BUILD_DIR=build
+
+# Flags
+GCC_FLAGS=(-std=gnu99 -ffreestanding -O2 -Wall -Wextra -Ilib/include -Ikernel -Wno-maybe-uninitialized)
+QEMU_FLAGS=(-soundhw pcspk)
+
+function KernelSubModule {
+	for file in $(find kernel/$1 -name '*.c'); do
+		echo "Compiling kernel source file: $file..."
+		$GCC -g -c $file -o $BUILD_DIR/"${file//\/}".o ${GCC_FLAGS[@]}
+	done
+}
+#########################################
 
 # Create build directory if it doesn't exist
 if ! [[ -d "$BUILD_DIR" ]]
@@ -12,8 +40,17 @@ then
 	touch $BUILD_DIR/boot/grub/grub.cfg
 fi
 
-args=("")
+# Overwrite grub.cfg
+echo "default=0
+timeout=15
+
+menuentry \"phiOS\" {
+	multiboot /boot/kernel.bin
+	boot
+}" > $BUILD_DIR/boot/grub/grub.cfg
+
 # Parse args
+args=("")
 for arg in "$@"; do
 	if [ $arg == "-iso" ]; then
 		args+=("-iso")
@@ -31,4 +68,19 @@ for arg in "$@"; do
 	fi
 done
 
+# Build kernel
 source "kernel/build.sh" ${args[@]}
+
+# Parse arguments
+for argm in "$arg"; do
+	if [ $argm == "-iso" ]; then
+		echo "Building ISO..."
+		grub-mkrescue -o phiOS.iso $BUILD_DIR
+	elif [ $argm == "-qemu" ]; then
+		read -p "Press ENTER to run qemu"
+		$QEMU -kernel $BUILD_DIR/boot/kernel.bin ${QEMU_FLAGS[@]}
+	fi
+done
+
+# Clean build directory
+rm $BUILD_DIR/*.o
