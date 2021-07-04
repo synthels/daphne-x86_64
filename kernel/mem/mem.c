de48c8eaea17823f952494b8605fe87ad422cde1
synthels <synthels.me@gmail.com>
2021-07-04 20:38:49 +0300

    mem: port mem

--- kernel/mem/mem.c
+++ kernel/mem/mem.c
@@ -22,10 +22,10 @@ static memsp_t memsp;
 void mem_init(void *mmap_ptr, uint64_t size)
 {
 	/* Validate mmap */
-    mmap_entry_t *mmap = (mmap_entry_t *) &mmap_ptr;
-    uint16_t memsp_index = 0; 
+	mmap_entry_t *mmap = (mmap_entry_t *) &mmap_ptr;
+	uint16_t memsp_index = 0; 
 	for (uint64_t i = 0; i < size; i++) {
-        mmap_entry_t *entry  = &mmap[i];
+		mmap_entry_t *entry  = &mmap[i];
 
 		/* 0 length entries */
 		if (entry->length == 0x0) {
@@ -35,17 +35,17 @@ void mem_init(void *mmap_ptr, uint64_t size)
 		/* Overlapping entries */
 		for (uint64_t j = 0; i < memsp.size; j++) {
 			/* Entry overlaps with another */
-            mmap_entry_t *memspe = &(memsp.mmap[j]);
+			mmap_entry_t *memspe = &(memsp.mmap[j]);
 			if (!(entry->base < memspe->base) && 
 				(entry->base < (memspe->base + memspe->length))) {
 				entry->type = INVALID;
 			}
 		}
 
-        /* Add to memsp */
-        if ((entry->type != INVALID) || (entry->type == USABLE)) {
-            memsp.mmap[memsp_index] = *entry;
-            ++memsp.size;
-        }
+		/* Add to memsp */
+		if ((entry->type != INVALID) || (entry->type == USABLE)) {
+			memsp.mmap[memsp_index] = *entry;
+			++memsp.size;
+		}
 	}
 }
