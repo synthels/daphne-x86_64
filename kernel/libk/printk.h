88a930a147bae6c9a5480898ab785ad45713c203
synthels <synthels.me@gmail.com>
2021-08-05 14:39:15 +0300

    printk: use correct log levels

--- kernel/libk/printk.h
+++ kernel/libk/printk.h
@@ -59,7 +59,7 @@ int get_log_level(void);
 
 #define ok(fmt, ...)    printk(NORMAL, "%s[  ok  ]%s " fmt "\n", KERN_COLOR_SUCCESS, KERN_COLOR_RESET, ##__VA_ARGS__)
 #define info(fmt, ...)  printk(NORMAL, "%s[ info ]%s " fmt "\n", KERN_COLOR_INFO, KERN_COLOR_RESET, ##__VA_ARGS__)
-#define warn(fmt, ...)  printk(NORMAL, "%s[ warn ]%s " fmt "\n", KERN_COLOR_WARN, KERN_COLOR_RESET, ##__VA_ARGS__)
-#define err(fmt, ...)   printk(NORMAL, "%s[ error ]%s " fmt "\n", KERN_COLOR_ERR, KERN_COLOR_RESET, ##__VA_ARGS__)
+#define warn(fmt, ...)  printk(WARNING, "%s[ warn ]%s " fmt "\n", KERN_COLOR_WARN, KERN_COLOR_RESET, ##__VA_ARGS__)
+#define err(fmt, ...)   printk(ERROR, "%s[ error ]%s " fmt "\n", KERN_COLOR_ERR, KERN_COLOR_RESET, ##__VA_ARGS__)
 
 #endif
