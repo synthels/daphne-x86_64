f80e495b9e70df3e6c56209ffe945cd00bc4696e
synthels <synthels.me@gmail.com>
2022-02-08 00:15:11 +0200

    x86_64: update limine?

--- src/kernel/arch/x86_64/limine/stage23/lib/trace.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LIB__TRACE_H__
-#define __LIB__TRACE_H__
-
-#include <stddef.h>
-
-void print_stacktrace(size_t *base_ptr);
-
-#endif
--- src/kernel/arch/x86_64/limine/stage23/protos/stivale2.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PROTOS__STIVALE2_H__
-#define __PROTOS__STIVALE2_H__
-
-#include <stdbool.h>
-
-void stivale2_load(char *config, char *cmdline, bool pxe, void *efi_system_table);
-
-#endif
